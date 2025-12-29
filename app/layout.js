fc199edb301e26e702920a3afd1e707caf1e9934
Abchoudhary2512 <bhakarajay033@gmail.com>
2025-12-29 21:33:43 +0530

    add: created the Landing page

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+
+export const metadata = {
+  title: "Price Tracker - Never Miss a Price Drop",
+  description:
+    "Track product prices across e-commerce sites and get alerts on price drops",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
